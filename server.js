a3f3f68c25c0838aaf4677af93904afa815b61a4
Jack Meuwissen <jmeuwissen42@gmail.com>
2019-09-18 13:57:15 -0500

    copypasta from working starwars app and changed variable names

--- server.js
+++ server.js
@@ -12,3 +12,73 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+
+// Star Wars reservations (DATA)
+// =============================================================
+var reservations = [
+    {
+      routeName: "yoda",
+      name: "Yoda",
+      role: "Jedi Master",
+      age: 900,
+      forcePoints: 2000
+    },
+  ];
+  
+  // Routes
+  // =============================================================
+  
+  // Basic route that sends the user first to the AJAX Page
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "view.html"));
+  });
+  
+  app.get("/add", function(req, res) {
+    res.sendFile(path.join(__dirname, "add.html"));
+  });
+  
+  // Displays all reservations
+  app.get("/api/reservations", function(req, res) {
+    return res.json(reservations);
+  });
+  
+  // Displays a single character, or returns false
+  app.get("/api/reservations/:character", function(req, res) {
+    var chosen = req.params.character;
+  
+    console.log(chosen);
+  
+    for (var i = 0; i < reservations.length; i++) {
+      if (chosen === reservations[i].routeName) {
+        return res.json(reservations[i]);
+      }
+    }
+  
+    return res.json(false);
+  });
+  
+  // Create New reservations - takes in JSON input
+  app.post("/api/reservations", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newReservation = req.body;
+    console.log("newReservation", newReservation)
+  
+    // Using a RegEx Pattern to remove spaces from newReservation
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newReservation);
+  
+    reservations.push(newReservation);
+  
+    res.json(newReservation);
+  });
+  
+  // Starts the server to begin listening
+  // =============================================================
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
