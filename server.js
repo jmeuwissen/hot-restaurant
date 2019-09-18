b97e91ff1937af14d036b644bc5f5b5b3532d8fd
Jack Meuwissen <jmeuwissen42@gmail.com>
2019-09-18 14:18:34 -0500

    updated routes with basic get requests

--- server.js
+++ server.js
@@ -29,35 +29,26 @@ var reservations = [
   // Routes
   // =============================================================
   
-  // Basic route that sends the user first to the AJAX Page
+  //route for home page
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "view.html"));
+    res.sendFile(path.join(__dirname, "home.html"));
   });
   
-  app.get("/add", function(req, res) {
-    res.sendFile(path.join(__dirname, "add.html"));
+
+  //route for reserve page
+  app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
   });
   
-  // Displays all reservations
-  app.get("/api/reservations", function(req, res) {
-    return res.json(reservations);
+
+  //route for tables page
+  //will display all reservations
+  app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
   });
-  
-  // Displays a single character, or returns false
-  app.get("/api/reservations/:character", function(req, res) {
-    var chosen = req.params.character;
-  
-    console.log(chosen);
-  
-    for (var i = 0; i < reservations.length; i++) {
-      if (chosen === reservations[i].routeName) {
-        return res.json(reservations[i]);
-      }
-    }
-  
-    return res.json(false);
-  });
-  
+
+
+
   // Create New reservations - takes in JSON input
   app.post("/api/reservations", function(req, res) {
     // req.body hosts is equal to the JSON post sent from the user
